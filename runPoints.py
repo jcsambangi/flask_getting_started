c3a148a337bb67581d9a86b7f03d96f46e063e8d
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-11-11 21:33:22 -0500

    Ran server from VM, test passed

--- runPoints.py
+++ runPoints.py
@@ -7,10 +7,15 @@ def points():
              "a":[2, 2],
              "b":[5, 6]
     }
-    r = requests.post("http://127.0.0.1:5000/distance", json=points)
+    r = requests.post("http://vcm-7315.vm.duke.edu:5000/distance", json=points)
     distance = r.json()
     print(distance)
 
+def other():
+    print(requests.get("http://vcm-7315.vm.duke.edu:5000/name").json())
+    print(requests.get("http://vcm-7315.vm.duke.edu:5000/hello/Jaydeep").json())
 
+    
 if __name__ == "__main__":
     points()
+    other()
